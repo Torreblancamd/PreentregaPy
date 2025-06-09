f15f7542e8f376badd0c577bec339bcb8c71e3a5
Maximiliano Torreblanca <torreblancamd@gmail.com>
2025-06-09 20:49:15 -0300

    v1

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", views.inicio),
@@ -10,6 +11,12 @@ urlpatterns = [
     #path("alta_curso/<nombre>" , views.alta_curso)
     path("alta_curso", views.curso_formulario),
     path("buscar_curso",views.buscar_curso),
-    path("buscar",views.buscar)
+    path("buscar",views.buscar),
+    path("elimina_curso/<int:id>", views.elimina_curso, name="elimina_curso"),
+    path("editar_curso/<int:id>", views.editar , name="editar_curso"),
+    path("login" , views.login_request , name="Login"),
+    path("register", views.register , name="Register"),
+    path("logout", LogoutView.as_view(template_name="logout.html") , name="Logout"),
+    path("editarPerfil", views.editarPerfil , name="editarPerfil")
 
 ]
--- app_coder/views.py
+++ app_coder/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from app_coder.models import Curso
 from django.template import loader
-from app_coder.forms import Curso_formulario
+from app_coder.forms import Curso_formulario, UserEditForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
@@ -10,6 +14,8 @@ def inicio(request):
 
     return render( request , "padre.html" )
 
+
+@login_required
 def cursos(request):
     cursos = Curso.objects.all()
     dicc = {"cursos" : cursos}
@@ -76,3 +82,103 @@ def buscar(request):
         return render(request, "resultado_busqueda.html", {"cursos":curso})
     else:
         return HttpResponse("Campo vacio")
+    
+
+
+
+
+def elimina_curso(request,id):
+    curso = Curso.objects.get(id=id)
+    curso.delete()
+    curso = Curso.objects.all()
+    return render(request, "cursos.html" , {"cursos":curso})
+
+
+
+def editar(request , id):
+    curso = Curso.objects.get(id=id)
+
+    if request.method == "POST":
+        
+        mi_formulario = Curso_formulario( request.POST )
+        if mi_formulario.is_valid():
+            datos = mi_formulario.cleaned_data
+            curso.nombre = datos["nombre"]
+            curso.camada = datos["camada"]
+            curso.save()
+
+            curso = Curso.objects.all()
+            return render(request, "cursos.html", {"cursos":curso})
+
+
+    else:
+        mi_formulario = Curso_formulario(initial={"nombre":curso.nombre , "camada":curso.camada})
+    return render(request, "editar_curso.html", {"mi_formulario":mi_formulario, "curso":curso})
+
+
+
+
+
+
+def login_request(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contra = form.cleaned_data.get("password")
+
+            user = authenticate(username=usuario, password=contra)
+
+            if user is not None:
+                login(request,user)
+                return render(request, "inicio.html" , {"mensaje":f"Bienvenido/a: {usuario}"})
+            else:
+                return HttpResponse("Usuario no encontrado")
+
+    
+    form = AuthenticationForm()
+    return render(request , "login.html", {"form":form})
+
+
+
+
+
+
+def register(request):
+
+    if request.method == "POST":        
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Usuario creado")
+    else:
+        form = UserCreationForm()
+    return render(request, "registro.html", {"form":form})
+
+
+
+
+def editarPerfil(request):
+
+    usuario = request.user
+    
+    if request.method == "POST":
+        
+        miFormulario = UserEditForm(request.POST)
+
+        if miFormulario.is_valid():
+             
+            informacion = miFormulario.cleaned_data
+            usuario.email = informacion["email"]
+            password = informacion["password1"]
+            usuario.set_password(password)
+            usuario.save()
+
+            return render(request, "inicio.html")
+
+
+    else:
+        miFormulario = UserEditForm(initial={"email":usuario.email})
+    
+    return render(request , "editar_perfil.html", {"miFormulario":miFormulario , "usuario":usuario})
